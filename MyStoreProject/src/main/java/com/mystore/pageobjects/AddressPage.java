3458f2333d759328616b9e1aaf9c29471c131b2b
Sand2812 <biswabhusanbehera05@gmail.com>
2024-03-31 12:50:17 +0530

    first commit

---
+++ MyStoreProject/src/main/java/com/mystore/pageobjects/AddressPage.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.mystore.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.mystore.actiondriver.MyAction;
+import com.mystore.base.BaseClass;
+
+/**
+ * @author Hitendra
+ *
+ */
+public class AddressPage extends BaseClass {
+	
+	MyAction action= new MyAction();
+	
+	@FindBy(xpath="//span[text()='Proceed to checkout']")
+	private WebElement proceedToCheckOut;
+	
+	public AddressPage() {
+		PageFactory.initElements(getDriver(), this);
+	}
+
+	public ShippingPage clickOnCheckOut() throws Throwable {
+		action.click(getDriver(), proceedToCheckOut);
+		return new ShippingPage();
+	}
+	
+}
